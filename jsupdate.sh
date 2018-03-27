e0a2cbdb0117658661d0b50da7e5de42edd34fc6
dfsklar <dfsklar@gmail.com>
2018-03-27 07:00:48 -0400

    Perform npm install only if necessary

--- jsupdate.sh
+++ jsupdate.sh
@@ -1,15 +1,28 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash                                                                                                                        
 
-# ONLY FOR UPDATING FORUM JS!!
+# ONLY FOR UPDATING FORUM JS!!                                                                                                             
 
-# does not update admin JS
+# does not update admin JS                                                                                                                 
 
-# This script compiles Flarum's core assets so that they can be used in-browser.
-# It should be run from the root directory of the core.
+# This script compiles this extenion's assets so that they can be used in-browser.                                                           
+# It should be run from the root directory of the extension.                                                                                    
 
-/bin/rm -f ../../../assets/forum*.js
 
 base=$PWD
-
 cd "${base}/js/forum"
-gulp
+if [ ! -d node_modules ]
+then
+    npm install
+fi
+
+gulp || exit 1
+
+if [ -d  "$FLARUMASSETDIR" ]
+then
+    /bin/rm -f $FLARUMASSETDIR/forum*.js
+    /bin/rm -f $FLARUMASSETDIR/*.js  $FLARUMASSETDIR/rev*
+else
+    echo "Missing or incorrect environment variable FLARUMASSETDIR."
+    exit 1
+fi
+
